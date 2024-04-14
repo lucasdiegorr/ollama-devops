a86d288463386e9ee85340080ca61d5884fc88d7
Lucas Rocha <lucas.diegorr@gmail.com>
2024-04-14 20:55:59 -0300

    Ollama docker script;

---
+++ docker/ollama-docker.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Run Ollama without GPU
+docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+
+# Run Ollama without GPU
+docker run -d --gpus=all -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
+
+# Run model
+docker exec -it ollama ollama run gemma:2b
+
+# Run Web UI in Docker Desktop
+docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
+
+# Run Web UI in the server
+# docker run -d -p 3000:8080 -e OLLAMA_BASE_URL=https://example.com -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
